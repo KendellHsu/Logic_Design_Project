b1530680f84638582a244d4d014b2801ea40d3a4
ivan125126 <ivan125126@gmail.com>
2024-06-21 22:27:09 +0800

    update output panel

--- LED_matrix_test/src/outputPenel.v
+++ LED_matrix_test/src/outputPenel.v
@@ -21,6 +21,18 @@ Others aren't beaming.
 module matrix (
     input clk,
     input rst,                //positive edge
+    input [1:0] state,
+    input [6143:0] menuMap,
+    input [191:0] scoreMap0,
+    input [191:0] scoreMap1,
+    input [191:0] scoreMap2,
+    input [191:0] scoreMap3,
+    input [191:0] scoreMap4,
+    input [191:0] scoreMap5,
+    input [191:0] scoreMap6,
+    input [191:0] scoreMap7,
+    input [191:0] scoreMap8,
+    input [191:0] scoreMap9,
     input [191:0] notesMap0,
     input [191:0] notesMap1,
     input [191:0] notesMap2,
@@ -47,7 +59,7 @@ reg [1:0] CS, NS;
 reg [6:0] col;    // column count
 reg [3:0] row;    // row count
 
-
+parameter MENU = 2'd0, PLAY = 2'd1, FINISH = 2'd2;
 parameter IDLE = 2'd0, GET = 2'd1, TRANSMIT = 2'd2;
 
     //FSM
@@ -108,41 +120,216 @@ parameter IDLE = 2'd0, GET = 2'd1, TRANSMIT = 2'd2;
             B1 <= 1'd0;
         end
 
-        else if(row == 0) begin    
-            R1 <= notesMap0[col*3+2];
-            G1 <= notesMap0[col*3+1];
-            B1 <= notesMap0[col*3];
-        end
-        else if(row == 1) begin    
-            R1 <= notesMap1[col*3+2];
-            G1 <= notesMap1[col*3+1];
-            B1 <= notesMap1[col*3];
-        end
-        else if(row == 2) begin    
-            R1 <= notesMap2[col*3+2];
-            G1 <= notesMap2[col*3+1];
-            B1 <= notesMap2[col*3];
-        end
-        else if(row == 3) begin    
-            R1 <= notesMap3[col*3+2];
-            G1 <= notesMap3[col*3+1];
-            B1 <= notesMap3[col*3];
-        end
-        else if(row == 4) begin    
-            R1 <= notesMap4[col*3+2];
-            G1 <= notesMap4[col*3+1];
-            B1 <= notesMap4[col*3];
-        end
-        else if(row == 5) begin    
-            R1 <= notesMap5[col*3+2];
-            G1 <= notesMap5[col*3+1];
-            B1 <= notesMap5[col*3];
-        end
-        else if(row == 6) begin    
-            R1 <= notesMap6[col*3+2];
-            G1 <= notesMap6[col*3+1];
-            B1 <= notesMap6[col*3];
-        end
+        else 
+        case (state)
+            MENU:
+            begin
+                R0 <= menuMap[6143-(row*64+col)*3];
+                G0 <= menuMap[6142-(row*64+col)*3];
+                B0 <= menuMap[6141-(row*64+col)*3];
+                R0 <= menuMap[3071-(row*64+col)*3];
+                G0 <= menuMap[3070-(row*64+col)*3];
+                B0 <= menuMap[3069-(row*64+col)*3];
+            end
+            PLAY:
+            begin
+                if(row == 3) begin    
+                    R0 <= scoreMap0[191-col*3];
+                    G0 <= scoreMap0[190-col*3];
+                    B0 <= scoreMap0[189-col*3];
+                    if (col==7b'6) begin
+                        R1 <= 1'b1;
+                        G1 <= 1'b1;
+                        B1 <= 1'b0;
+                    end
+                    else begin
+                        R1 <= 1'b0;
+                        G1 <= 1'b0;
+                        B1 <= 1'b0;
+                    end
+                end
+                else if(row == 4) begin    
+                    R0 <= scoreMap1[191-col*3];
+                    G0 <= scoreMap1[190-col*3];
+                    B0 <= scoreMap1[189-col*3];
+                    if (col==7b'6) begin
+                        R1 <= 1'b1;
+                        G1 <= 1'b1;
+                        B1 <= 1'b0;
+                    end
+                    else begin
+                        R1 <= 1'b0;
+                        G1 <= 1'b0;
+                        B1 <= 1'b0;
+                    end
+                end
+                else if(row == 5) begin    
+                    R0 <= scoreMap2[191-col*3];
+                    G0 <= scoreMap2[190-col*3];
+                    B0 <= scoreMap2[189-col*3];
+                    R1 <= notesMap0[col*3+2];
+                    G1 <= notesMap0[col*3+1];
+                    B1 <= notesMap0[col*3];
+                end
+                else if(row == 6) begin    
+                    R0 <= scoreMap3[191-col*3];
+                    G0 <= scoreMap3[190-col*3];
+                    B0 <= scoreMap3[189-col*3];
+                    R1 <= notesMap1[col*3+2];
+                    G1 <= notesMap1[col*3+1];
+                    B1 <= notesMap1[col*3];
+                end
+                else if(row == 7) begin    
+                    R0 <= scoreMap4[191-col*3];
+                    G0 <= scoreMap4[190-col*3];
+                    B0 <= scoreMap4[189-col*3];
+                    R1 <= notesMap2[col*3+2];
+                    G1 <= notesMap2[col*3+1];
+                    B1 <= notesMap2[col*3];
+                end
+                else if(row == 8) begin    
+                    R0 <= scoreMap5[191-col*3];
+                    G0 <= scoreMap5[190-col*3];
+                    B0 <= scoreMap5[189-col*3];
+                    R1 <= notesMap3[col*3+2];
+                    G1 <= notesMap3[col*3+1];
+                    B1 <= notesMap3[col*3];
+                end
+                else if(row == 9) begin    
+                    R0 <= scoreMap6[191-col*3];
+                    G0 <= scoreMap6[190-col*3];
+                    B0 <= scoreMap6[189-col*3];
+                    R1 <= notesMap4[col*3+2];
+                    G1 <= notesMap4[col*3+1];
+                    B1 <= notesMap4[col*3];
+                end
+                else if(row == 10) begin    
+                    R0 <= scoreMap7[191-col*3];
+                    G0 <= scoreMap7[190-col*3];
+                    B0 <= scoreMap7[189-col*3];
+                    R1 <= notesMap5[col*3+2];
+                    G1 <= notesMap5[col*3+1];
+                    B1 <= notesMap5[col*3];
+                end
+                else if(row == 11) begin    
+                    R0 <= scoreMap8[191-col*3];
+                    G0 <= scoreMap8[190-col*3];
+                    B0 <= scoreMap8[189-col*3];
+                    R1 <= notesMap6[col*3+2];
+                    G1 <= notesMap6[col*3+1];
+                    B1 <= notesMap6[col*3];
+                end
+                else if(row == 12) begin    
+                    R0 <= scoreMap9[191-col*3];
+                    G0 <= scoreMap9[190-col*3];
+                    B0 <= scoreMap9[189-col*3];
+                    if (col==7b'6) begin
+                        R1 <= 1'b1;
+                        G1 <= 1'b1;
+                        B1 <= 1'b0;
+                    end
+                    else begin
+                        R1 <= 1'b0;
+                        G1 <= 1'b0;
+                        B1 <= 1'b0;
+                    end
+                end
+                else if(row == 0) begin    
+                    R1 <= 1'b1;
+                    G1 <= 1'b0;
+                    B1 <= 1'b1;
+                    R0 <= 1'b0;
+                    G0 <= 1'b0;
+                    B0 <= 1'b0;
+                end
+                else begin
+                    R0 <= 1'b0;
+                    G0 <= 1'b0;
+                    B0 <= 1'b0;
+                    if (col==7b'6) begin
+                        R1 <= 1'b1;
+                        G1 <= 1'b1;
+                        B1 <= 1'b0;
+                    end
+                    else begin
+                        R1 <= 1'b0;
+                        G1 <= 1'b0;
+                        B1 <= 1'b0;
+                    end
+                end
+            end
+            FINISH: 
+            begin
+                if(row == 3) begin    
+                    R0 <= scoreMap0[191-col*3];
+                    G0 <= scoreMap0[190-col*3];
+                    B0 <= scoreMap0[189-col*3];
+                end
+                else if(row == 4) begin    
+                    R0 <= scoreMap1[191-col*3];
+                    G0 <= scoreMap1[190-col*3];
+                    B0 <= scoreMap1[189-col*3];
+                end
+                else if(row == 5) begin    
+                    R0 <= scoreMap2[191-col*3];
+                    G0 <= scoreMap2[190-col*3];
+                    B0 <= scoreMap2[189-col*3];
+                end
+                else if(row == 6) begin    
+                    R0 <= scoreMap3[191-col*3];
+                    G0 <= scoreMap3[190-col*3];
+                    B0 <= scoreMap3[189-col*3];
+                end
+                else if(row == 7) begin    
+                    R0 <= scoreMap4[191-col*3];
+                    G0 <= scoreMap4[190-col*3];
+                    B0 <= scoreMap4[189-col*3];
+                end
+                else if(row == 8) begin    
+                    R0 <= scoreMap5[191-col*3];
+                    G0 <= scoreMap5[190-col*3];
+                    B0 <= scoreMap5[189-col*3];
+                end
+                else if(row == 9) begin    
+                    R0 <= scoreMap6[191-col*3];
+                    G0 <= scoreMap6[190-col*3];
+                    B0 <= scoreMap6[189-col*3];
+                end
+                else if(row == 10) begin    
+                    R0 <= scoreMap7[191-col*3];
+                    G0 <= scoreMap7[190-col*3];
+                    B0 <= scoreMap7[189-col*3];
+                end
+                else if(row == 11) begin    
+                    R0 <= scoreMap8[191-col*3];
+                    G0 <= scoreMap8[190-col*3];
+                    B0 <= scoreMap8[189-col*3];
+                end
+                else if(row == 12) begin    
+                    R0 <= scoreMap9[191-col*3];
+                    G0 <= scoreMap9[190-col*3];
+                    B0 <= scoreMap9[189-col*3];
+                end
+                else begin
+                    R0 <= 1'b0;
+                    G0 <= 1'b0;
+                    B0 <= 1'b0;
+                end
+                R1 <= 1'b0;
+                G1 <= 1'b0;
+                B1 <= 1'b0;
+            end
+            default: 
+            begin
+                R0 <= 1'b0;
+                G0 <= 1'b0;
+                B0 <= 1'b0;
+                R1 <= 1'b0;
+                G1 <= 1'b0;
+                B1 <= 1'b0;
+            end
+        endcase
+        
     end
 
     //OE, LAT output
